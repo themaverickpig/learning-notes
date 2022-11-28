22c5b82ae6b431d9adadff0c2fc6d969f889caf6
themaverickpig <1062539080@qq.com>
2022-11-29 01:14:48 +0800

    add js-notes

---
+++ JS-notes/2.type.js
@@ -0,0 +1,18 @@
+// let  a = 8;
+// console.log(Math.pow(a,1/3))
+// let b = Math.log(a)/Math.LN2
+// console.log(b)
+// console.log(Number.MAX_VALUE)
+
+// let a = 0/0;
+// console.log(a===NaN) //false
+// console.log(Number.isNaN(a)) //true
+
+let date = Date.now();
+let now = new Date();
+let ms = now.getTime();
+let iso = now.toISOString();
+
+console.log(ms)
+console.log(iso)
+console.log(date)
